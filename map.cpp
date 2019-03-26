ef7975fc6123effcd1cb74fa33efe80afe997449
ayushnagar123 <an431999@gmail.com>
2019-03-27 00:25:48 +0530

    update

--- map.cpp
+++ map.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 int main()
 {
-	int k;
-	cin>>k;
-	cout<<k;
-	return 0;
+	int n;
+	cin>>n;
+	int map[n][n];
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			cin>>map[i][j];
+		}
+	}
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			cout<<map[i][j]<<" ";
+		}
+		cout<<endl;
+	}
 }

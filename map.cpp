609f5c15eca4d390446225b0a0ab57da5bb85d23
aksharanigam1112 <aksharanigam111298@gmail.com>
2019-05-01 23:37:08 +0530

    benefit

--- map.cpp
+++ map.cpp
@@ -3,7 +3,7 @@
 #include<cstdlib>
 #include<fstream>
 #include "dotupdate.cpp"
-
+#include<stdio.h>
 #define N 15
 #define INF 99999
 using namespace std;
@@ -18,25 +18,21 @@ void printPath(double path[][N], int v, int u)
     cout << path[v][u] << " ";
 }
 
-// Function to print the shortest cost with path
-// information between all pairs of vertices
-void printSolution(double cost[N][N], double path[N][N] ,double distance[N][N],int i , int j)
+void printSolution(double cost[N][N], double path[N][N],int i , int j)
 {
     for (int v = 0; v < N; v++)
     {
         for (int u = 0; u < N; u++)
         {
             if (cost[v][u] == INF)
-                cout << " INF";
+                cout << "\tINF";
             else
-                cout << cost[v][u]<<" ";
+                printf("%.2f\t",cost[v][u]);
         }
         cout << endl;
     }
 
-    cout << endl;
-
-    for(int v=0;v<N;v++)
+  /*  for(int v=0;v<N;v++)
     {
         for(int u=0;u<N;u++)
         {
@@ -46,125 +42,115 @@ void printSolution(double cost[N][N], double path[N][N] ,double distance[N][N],i
                 cout<<distance[v][u]<<" ";
         }
         cout<<endl;
-
-    }
-    cout<<endl;
-
+    }*/
+    cout<<"The min cost path:- ";
     printPath(path, i, j);
 }
 
-// Function to run Floyd-Warshell algorithm
 void FloydWarshell(double adjMatrix[][N],double distance[][N],int i,int j)
 {
-    // cost[] and parent[] stores shortest-path
-    // (shortest-cost/shortest route) information
     double cost[N][N], path[N][N] ,dist_path[N][N];
+    double temp1 , temp2;
 
-    // initialize cost[] and parent[]
     for (int v = 0; v < N; v++)
     {
         for (int u = 0; u < N; u++)
         {
-            // initally cost would be same as weight
-            // of the edge
             cost[v][u] = adjMatrix[v][u];
 
             if (v == u)
-                {path[v][u] = 0;dist_path[v][u]=0;}
+                {
+                    path[v][u] = 0;
+                    dist_path[v][u]=0;
+                }
             else if (cost[v][u] != INF)
-                {path[v][u] = v;dist_path[v][u]=v;}
+                {
+                    path[v][u] = v;
+                    dist_path[v][u]=v;
+                }
             else
-                {path[v][u] = -1;dist_path[v][u]=-1;}
+                {
+                    path[v][u] = -1;
+                    dist_path[v][u]=-1;
+                }
         }
     }
-   
-    // run Floyd-Warshell
+
     for (int k = 0; k < N; k++)
     {
         for (int v = 0; v < N; v++)
         {
             for (int u = 0; u < N; u++)
             {
-                // If vertex k is on the shortest path from v to u,
-                // then update the value of cost[v][u], path[v][u]
+                temp1 = cost[v][k] + cost[k][u] ;
+                temp2 = distance[v][k]+distance[k][u];
 
-                if (cost[v][k] != INF && cost[k][u] != INF && cost[v][k] + cost[k][u] < cost[v][u])
+                if (cost[v][k] != INF && cost[k][u] != INF &&  distance[v][k]!=INF && distance[u][k]!=INF && temp1*temp2 < cost[v][u]*distance[v][u] )
                 {
                     cost[v][u] = cost[v][k] + cost[k][u];
+                    distance[v][u]=distance[v][k]+distance[k][u];
                     path[v][u] = path[k][u];
+                    dist_path[v][u]=dist_path[k][u];
                 }
-            if(distance[v][k]!=INF && distance[u][k]!=INF && distance[v][u]>distance[v][k]+distance[k][u])
+           /* if(distance[v][k]!=INF && distance[u][k]!=INF && distance[v][u]>distance[v][k]+distance[k][u])
                 {
                     distance[v][u]=distance[v][k]+distance[k][u];
                     dist_path[v][u]=dist_path[k][u];
-                }
+                }*/
             }
-
-            // if diagonal elements become negative, the
-            // graph contains a negative weight cycle
         }
     }
-
-    // Print the shortest path between all pairs of vertices
-    printSolution(cost, path,distance,i,j);
-    cout<<"The distance path";
+    printSolution(cost, path,i,j);
+    cout<<"The min distance path:- ";
     printPath(dist_path,i,j);
+    cout<<endl;
 }
-
-
 
 int main()
 {
-    
+
     int n;
     cin>>n;
-   // int temp=0;
-    //cout<<n<<endl;
-        double adjMatrix[N][N] ,distance[N][N],adjMatrix2[N][N];
-  
-   int V=N;
+    double adjMatrix[N][N] ,distance[N][N],adjMatrix2[N][N];
 
-     
-   for(int i=0;i<V;i++)
+   for(int i=0;i<N;i++)
     {
-        for(int j=0;j<V;j++)
+        for(int j=0;j<N;j++)
         {
             cin>>adjMatrix[i][j];
             adjMatrix2[i][j]=adjMatrix[i][j];
-            if(adjMatrix[i][j]==2 || adjMatrix[i][j]==1)adjMatrix2[i][j]=INF;
-            //if(adjMatrix[i][j]==1)adjMatrix2[i][j]=-1;
-        
-            
-        }
 
+            if(adjMatrix[i][j]==2 || adjMatrix[i][j]==1)
+                    adjMatrix2[i][j]=INF;
+            //if(adjMatrix[i][j]==1)adjMatrix2[i][j]=-1;
+        }
   }
 
- for(int i=0;i<V;i++)
+ for(int i=0;i<N;i++)
   {
-    for(int j=0;j<V;j++)
+    for(int j=0;j<N;j++)
     {
-           
+
            cin>>distance[i][j];
-           if(distance[i][j]==1000 || adjMatrix2[i][j]==INF)distance[i][j]=INF;
+           if(distance[i][j]==1000 || adjMatrix2[i][j]==INF)
+                distance[i][j]=INF;
            //if(adjMatrix2[i][j]==-1)distance[i][j]=-1;
     }
   }
    int can=0;
    //for(;;){
-   for(int i=0;i<V;i++)
+   for(int i=0;i<N;i++)
     {
-        for(int j=0;j<V;j++)
+        for(int j=0;j<N;j++)
         {
             if(distance[i][j]!=INF || adjMatrix2[i][j]!=INF)
-            adjMatrix2[i][j]=(distance[i][j])/((int)(10-10*adjMatrix2[i][j]));       
-             
+                adjMatrix2[i][j]=(distance[i][j])/((int)(10-10*adjMatrix2[i][j]));
         }
-
   }
-   
+
    FloydWarshell(adjMatrix2,distance,7,12);
-   
-    
+
+
     ofstream fout;
     fout.open("output.dot");
     fout<<"digraph Test {"<<endl;
@@ -180,7 +166,7 @@ int main()
     fout<<"}";
     fout.close();
     system("dot -Tpng output.dot >out.dot.png");
-    //if(can==0)    
+    //if(can==0)
 	system("firefox index.html");
     //can++;
      /*for(int i=0;i<n;i++)
@@ -205,7 +191,7 @@ int main()
 			cout<<endl;
 		}
     }*/
-    
+
     return 0;
 }
 
